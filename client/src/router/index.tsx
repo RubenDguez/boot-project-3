29a70d56e0608350a72b71bf6420aae305f0d232
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-11-08 15:18:14 -0500

    update: router to include landing signup and login

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,22 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import { NotFound } from "../pages";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import { Authentication, Landing, NotFound } from '../pages';
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <NotFound />,
-      children: [
-      ]
-    },
-    {
-      path: '/app',
-      element: <App />,
-      errorElement: <NotFound />,
-      children: [
-      ]
-    },
-  ])
+  {
+    path: '/',
+    element: <Landing />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/signup',
+    element: <Authentication.SignUp />,
+  },
+  {
+    path: '/login',
+    element: <Authentication.Login />,
+  },
+
+  {
+    path: '/app',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [],
+  },
+]);
 
 export default router;
