d04f05d915c444c145683e48fb5c609c967f700f
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-11-08 15:17:34 -0500

    create: landing page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as Authentication from './Authentication';
+import Landing from './Landing/Landing';
 import NotFound from './NotFound/NotFound';
 
-export { Authentication, NotFound };
+export { Authentication, Landing, NotFound };
