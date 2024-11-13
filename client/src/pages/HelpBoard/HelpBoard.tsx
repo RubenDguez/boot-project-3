f8bf6ffdd97f96f2cb31b96269b246eb5de75689
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-11-13 13:56:40 -0500

    add: custom useAuth to the pages

--- client/src/pages/HelpBoard/HelpBoard.tsx
+++ client/src/pages/HelpBoard/HelpBoard.tsx
@@ -1,6 +1,8 @@
 import { Typography } from "@mui/material";
+import useAuth from "../../hooks/useAuth";
 
 export default function HelpBoard() {
+    useAuth();
     return (
         <Typography variant="h2">Help Board</Typography>
     )
