2a8abde47e5af3d56aaa036b57bc0048e3fa7dfd
Isaiah Capers <isaiah.capers@outlook.com>
2024-11-14 15:50:45 -0500

    event component created

--- client/src/pages/ServiceCalendar/ServiceCalendar.tsx
+++ client/src/pages/ServiceCalendar/ServiceCalendar.tsx
@@ -4,7 +4,8 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 import useAuth from '../../hooks/useAuth';
-
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid2';
 
 export default function ServiceCalendar() {
     const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
@@ -12,18 +13,26 @@ export default function ServiceCalendar() {
     useAuth();
     
   return (
-
-    <LocalizationProvider dateAdapter={AdapterDayjs} >
-       <StaticDateTimePicker orientation="landscape"
-       value={value}
-       onChange={(newValue) => setValue(newValue)}
-       slotProps={{
-        actionBar: {
+    < Grid container spacing={2}>
+      <Grid size={6}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} >
+         <StaticDateTimePicker orientation="landscape"
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+            slotProps={{
+              actionBar: {
             actions:["today","accept","cancel"],
-           },
-         }} 
-       />
-       
-    </LocalizationProvider>
+            },
+          }} 
+          />      
+        </LocalizationProvider>
+      </Grid>
+      <Grid size={6}>
+        <Stack >
+
+      </Stack>
+      </Grid>
+      
+    </Grid>
   );
-}   
+}
