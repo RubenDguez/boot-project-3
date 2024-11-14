07d2b4dfb8206d7674247cc5fb4d858019aad1d9
Isaiah Capers <isaiah.capers@outlook.com>
2024-11-14 12:02:52 -0500

    event adding stage prep

--- client/src/pages/Calendar/Calendar.tsx
+++ client/src/pages/Calendar/Calendar.tsx
@@ -1,11 +1,24 @@
+import * as React from 'react';
+import dayjs, { Dayjs, locale } from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-
+import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 export default function Calendar() {
+    const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+    console.log(value);
+    
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-       <DateCalendar />
+    <LocalizationProvider dateAdapter={AdapterDayjs} >
+       <StaticDateTimePicker orientation="landscape"
+       value={value}
+       onChange={(newValue) => setValue(newValue)}
+       slotProps={{
+        actionBar: {
+            actions:["today","accept","cancel"],
+           },
+         }} 
+       />
+       
     </LocalizationProvider>
   );
-}
+}   
