ee4224d3350c250a0afef0312a1d3884d8c81e7e
ZapApriendo <isaiah.capers@outlook.com>
2024-11-14 21:28:20 -0500

    adding event section, event card's complete

--- client/src/components/CalendarCard/CalendarCard.tsx
+++ client/src/components/CalendarCard/CalendarCard.tsx
@@ -4,25 +4,32 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 
-<Card>
+const CalendarCard = () => {
+  return (
+
+  <Card sx={{ backgroundColor: '#9AC171'}}>
     <CardContent>
-        <Typography gutterBottom variant="h3" component="div">
-          Chairty Event Title
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Event Time & Date
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Event Location
-        </Typography>  
-      </CardContent>
-      <CardActions>
-        <Button size="small">Delete</Button>
-        <Button size="small">Reschedule</Button>
-        <Button size="small">Mark Complete</Button>
-      </CardActions>
+      <Typography gutterBottom variant="h3" component="div">
+        Chairty Event Title
+      </Typography>
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        Event Time & Date
+      </Typography>
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        Event Location
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Delete</Button>
+      <Button size="small">Reschedule</Button>
+      <Button size="small">Mark Complete</Button>
+    </CardActions>
     <CardMedia>
-        <img src="https://source.unsplash.com/random" alt="random" />
-        {/* placeholder for charity image */}
+      <img src="https://source.unsplash.com/random" alt="random" />
+      {/* placeholder for charity image */}
     </CardMedia>
-</Card>
+  </Card>
+);
+};
+
+export default CalendarCard;
--- client/src/pages/ServiceCalendar/ServiceCalendar.tsx
+++ client/src/pages/ServiceCalendar/ServiceCalendar.tsx
@@ -6,33 +6,45 @@ import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 import useAuth from '../../hooks/useAuth';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid2';
+import CalendarCard from '../../components/CalendarCard/CalendarCard';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { IconButton } from '@mui/material';
+// import theme from '../../theme/index';
+import FormDialog from '../../components/CalendarCard/AddEvent';
 
 export default function ServiceCalendar() {
-    const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
-    console.log(value);
-    useAuth();
-    
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+  console.log(value);
+  // useAuth();
+
   return (
-    < Grid container spacing={2}>
+    < Grid container spacing={2} >
       <Grid size={6}>
         <LocalizationProvider dateAdapter={AdapterDayjs} >
-         <StaticDateTimePicker orientation="landscape"
+          <StaticDateTimePicker
+            orientation="landscape"
+            openTo='day'
             value={value}
             onChange={(newValue) => setValue(newValue)}
+            sx={{ backgroundColor: '#9AC171'}}
             slotProps={{
               actionBar: {
-            actions:["today","accept","cancel"],
-            },
-          }} 
-          />      
+                actions: ["today", "accept",],
+              },
+            }}
+          />
         </LocalizationProvider>
+        
       </Grid>
       <Grid size={6}>
-        <Stack >
-
-      </Stack>
+        <Stack spacing={1} >
+          <IconButton aria-label='add event' sx={{ color: '#E7DECD' }} onClick={FormDialog}>
+            <AddCircleIcon  fontSize='large' />
+          </IconButton>  
+          {/* <CalendarCard /> */}
+        </Stack>
       </Grid>
-      
+
     </Grid>
   );
 }
