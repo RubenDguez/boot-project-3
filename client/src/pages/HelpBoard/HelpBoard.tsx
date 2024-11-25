4016d499a5cd8ee9ef460ec2dff44883f152a6e9
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-11-24 21:32:58 -0500

    pushing the code

--- client/src/pages/HelpBoard/HelpBoard.tsx
+++ client/src/pages/HelpBoard/HelpBoard.tsx
@@ -29,123 +29,146 @@ interface HelpBoardVars {
   completedBy: string;
 }
 
-
 export default function HelpBoard() {
-  useAuth();
-  const { loading, error, data, refetch } = useQuery<HelpBoardData, HelpBoardVars>(
-    FIND_ALL_POST
-  );
-
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
-  const [status, setStatus] = useState('');
-  const [createdBy, setCreatedBy] = useState('');
-  const [completedBy, setCompletedBy] = useState('');
-
-  const [addHelpBoard] = useMutation(ADD_POST);
-  const [removeHelpBoard] = useMutation(REMOVE_HELP_BOARD);
-
-  const handleAdd = () => {
-    addHelpBoard({
-      variables: { input: { title, description, date, status, createdBy, completedBy } }
-    }).then(() => {
-      setTitle('');
-      setDescription('');
-      setDate('');
-      setStatus('');
-      setCreatedBy('');
-      setCompletedBy('');
-      refetch();
-    });
-  };
-
-  const handleRemove = (helpBoardId: string) => {
-    removeHelpBoard({
-      variables: { helpBoardId }
-    }).then(() => {
-      refetch();
-    });
-  };
+  const { loading , data} = useQuery(FIND_ALL_POST);
+  const helpBoards = data?.findAllPosts || [];
 
   return (
     <div>
       <Typography variant="h4">Help Board</Typography>
-      <TextField
-        label="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-        label="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-      label="Date"
-      value={date}
-      onChange={(e) => setDate(e.target.value)}
-      variant="outlined"
-      margin="normal"
-      type="date"
-      InputLabelProps={{
-        shrink: true,
-      }}
-       
-
-      />
-      <TextField
-        label="Status"
-        value={status}
-        onChange={(e) => setStatus(e.target.value)}
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-        label="Created By"
-        value={createdBy}
-        onChange={(e) => setCreatedBy(e.target.value)}
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-        label="Completed By"
-        value={completedBy}
-        onChange={(e) => setCompletedBy(e.target.value)}
-        variant="outlined"
-        margin="normal"
-      />
-      <Button variant="contained" color="primary" onClick={handleAdd}>
-        Add Post to Help Board  
-      </Button>
       {loading && <Typography>Loading...</Typography>}
-
-      {error && <Typography>Error: {error.message}</Typography>}
-
-      {!loading && !error && data && (
-        <div>
-          {data.helpBoards.map((helpBoard) => (
-            <Card key={helpBoard._id} sx={{ margin: '10px 0' }}>
-              <CardContent>
-                <Typography variant="h6">{helpBoard.title}</Typography>
-                <Typography>{helpBoard.description}</Typography>
-                <Typography>{helpBoard.date}</Typography>
-                <Typography>{helpBoard.status}</Typography>
-                <Typography>{helpBoard.createdBy}</Typography>
-                <Typography>{helpBoard.completedBy}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button onClick={() => handleRemove(helpBoard._id)}>Remove</Button>
-              </CardActions>
-            </Card>
-          ))}
-        </div>
-      )}
+      {helpBoards.map((helpBoard: HelpBoard) => (
+        <Card key={helpBoard._id} sx={{ margin: '10px 0' }}>
+          <CardContent>
+            <Typography variant="h6">{helpBoard.title}</Typography>
+            <Typography>{helpBoard.description}</Typography>
+            <Typography>{helpBoard.date}</Typography>
+            <Typography>{helpBoard.status}</Typography>
+            <Typography>{helpBoard.createdBy}</Typography>
+            <Typography>{helpBoard.completedBy}</Typography>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
-
 }
+
+// export default function HelpBoard() {
+//   useAuth();
+//   const { loading, error, data, refetch } = useQuery<HelpBoardData, HelpBoardVars>(
+//     FIND_ALL_POST
+//   );
+
+//   const [title, setTitle] = useState('');
+//   const [description, setDescription] = useState('');
+//   const [date, setDate] = useState('');
+//   const [status, setStatus] = useState('');
+//   const [createdBy, setCreatedBy] = useState('');
+//   const [completedBy, setCompletedBy] = useState('');
+
+//   const [addHelpBoard] = useMutation(ADD_POST);
+//   const [removeHelpBoard] = useMutation(REMOVE_HELP_BOARD);
+
+//   const handleAdd = () => {
+//     addHelpBoard({
+//       variables: { input: { title, description, date, status, createdBy, completedBy } }
+//     }).then(() => {
+//       setTitle('');
+//       setDescription('');
+//       setDate('');
+//       setStatus('');
+//       setCreatedBy('');
+//       setCompletedBy('');
+//       refetch();
+//     });
+//   };
+
+//   const handleRemove = (helpBoardId: string) => {
+//     removeHelpBoard({
+//       variables: { helpBoardId }
+//     }).then(() => {
+//       refetch();
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <Typography variant="h4">Help Board</Typography>
+//       <TextField
+//         label="Title"
+//         value={title}
+//         onChange={(e) => setTitle(e.target.value)}
+//         variant="outlined"
+//         margin="normal"
+//       />
+//       <TextField
+//         label="Description"
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//         variant="outlined"
+//         margin="normal"
+//       />
+//       <TextField
+//       label="Date"
+//       value={date}
+//       onChange={(e) => setDate(e.target.value)}
+//       variant="outlined"
+//       margin="normal"
+//       type="date"
+//       InputLabelProps={{
+//         shrink: true,
+//       }}
+       
+
+//       />
+//       <TextField
+//         label="Status"
+//         value={status}
+//         onChange={(e) => setStatus(e.target.value)}
+//         variant="outlined"
+//         margin="normal"
+//       />
+//       <TextField
+//         label="Created By"
+//         value={createdBy}
+//         onChange={(e) => setCreatedBy(e.target.value)}
+//         variant="outlined"
+//         margin="normal"
+//       />
+//       <TextField
+//         label="Completed By"
+//         value={completedBy}
+//         onChange={(e) => setCompletedBy(e.target.value)}
+//         variant="outlined"
+//         margin="normal"
+//       />
+//       <Button variant="contained" color="primary" onClick={handleAdd}>
+//         Add Post to Help Board  
+//       </Button>
+//       {loading && <Typography>Loading...</Typography>}
+
+//       {error && <Typography>Error: {error.message}</Typography>}
+
+//       {!loading && !error && data && (
+//         <div>
+//           {data.helpBoards.map((helpBoard) => (
+//             <Card key={helpBoard._id} sx={{ margin: '10px 0' }}>
+//               <CardContent>
+//                 <Typography variant="h6">{helpBoard.title}</Typography>
+//                 <Typography>{helpBoard.description}</Typography>
+//                 <Typography>{helpBoard.date}</Typography>
+//                 <Typography>{helpBoard.status}</Typography>
+//                 <Typography>{helpBoard.createdBy}</Typography>
+//                 <Typography>{helpBoard.completedBy}</Typography>
+//               </CardContent>
+//               <CardActions>
+//                 <Button onClick={() => handleRemove(helpBoard._id)}>Remove</Button>
+//               </CardActions>
+//             </Card>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+
+// }
